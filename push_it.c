0623ed81b6492b1ea2d00904dedc9a78a2b22686
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-12 20:48:55 +0000

    included line number in error message

--- push_it.c
+++ push_it.c
@@ -1,25 +1,26 @@
 #include "monty.h"
 
-#define USAGE_INT ("L<line_+number>: usage: push integer")
+#define USAGE_INT ("L%ui: usage: push integer")
 
 /**
  * push_it - pushes an element to the syack
  *
  * @stack: double pointer to head node of doubly linked list (the stack)
- * @line_number: 
+ * @line_number: line_number to print for errorws
  *
  * Return: Void
  */
 
 
-void push_it(stack_t **stack, unsigned int line_number)
+void push_it(stack_t **stack, unsigned int line_number, char *data)
 {
 	stack_t *node = malloc(sizeof(stack_t));
 	stack_t *searcher;
 
-	if ((!*stack) || (!stack) || (!node))
-		dprintf(STDERR_FILENO, USAGE_INT), exit(EXIT_FAILURE);
+	if ((!*stack) || (!stack) || (!node) || (!data))
+		dprintf(STDERR_FILENO, USAGE_INT, line_number), exit(EXIT_FAILURE);
 
+	
 	searcher = *stack
 
 	while (searcher)
@@ -28,6 +29,6 @@ void push_it(stack_t **stack, unsigned int line_number)
 	searcher->next = node;
 	node->prev = searcher;
 	node->next = NULL;
-	node->n = NULL;
+	node->n = atoi(data);
 
 }
